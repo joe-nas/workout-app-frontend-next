a8349193788f8be9e2803fcb569d412ac9ff72c4
joe-nas <pintac1984@gmail.com>
2023-11-10 13:52:29 +0100

    simplified edit profile functionality

---
+++ app/user/[oauthId]/profile/page.jsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { getUser } from '@/app/api/UserService';
+import UserProfileComponent from '@/components/UserProfileComponent';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+
+const UserProfile = () => {
+  const { data: session } = useSession();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const setUpUser = async () => {
+      if (session?.user.oauthId) {
+        try {
+          const currentUser = await getUser(session.user.oauthId);
+          setUser(currentUser); // Adjust this to be server-compatible
+          setLoading(false);
+        } catch (e) {
+          setLoading(true);
+          console.log(e);
+        }
+      }
+    }
+    setUpUser();
+  }, [session]);
+
+  return (
+    <div>
+      {loading && <div>Loading...</div>}
+      {!loading && < UserProfileComponent
+        username={user.username}
+        email={user.email}
+      />
+      }
+    </div>
+  );
+};
+
+export default UserProfile;
--- components/UserProfileComponent.jsx
+++ components/UserProfileComponent.jsx
@@ -2,37 +2,44 @@
 import { updateProfile } from "@/app/api/UserService";
 import { useForm } from "react-hook-form";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-const UserProfileComponent = ({ name, image, metric, email }) => {
+const UserProfileComponent = ({ username, email }) => {
   const { data: session } = useSession();
+  const router = useRouter();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (updatedData) => {
-    // updateProfile(oauthId, jwt, updatedData);
-    console.log(updatedData);
-  };
+  const onSubmit = async (updatedData) => {
+    if (session?.user.oauthId) {
+      try {
+        updateProfile(session.user.oauthId, session.user.jwt, updatedData);
+        router.reload();
+        console.log(updatedData);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
 
   return (
-    <div className="flex w-1/3 p-5">
+    <div className="flex mt-8 p-5 shadow-2xl shadow-black backdrop-opacity-90 bg-white/50 backdrop-blur-md rounded-lg">
       <form id="user-profile-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col">
           <InputField
             name="username"
             label="Username"
-            placeholder={name}
+            placeholder={username}
             register={register}
             id="username"
-          />
-          <InputField
-            name="image"
-            label="Profile Image"
-            placeholder={image}
-            register={register}
-            id="image"
+            validation={{
+              required: { value: true, message: "Username is required" },
+              minLength: { value: 1, message: "Username must be at least 1 character long" }
+            }}
+            errors={errors}
           />
           <InputField
             name="email"
@@ -40,6 +47,14 @@ const UserProfileComponent = ({ name, image, metric, email }) => {
             placeholder={email}
             register={register}
             id="email"
+            validation={{
+              required: { value: true, message: "Email is required" },
+              pattern: {
+                value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                message: "Please enter a valid email address"
+              }
+            }}
+            errors={errors}
           />
           <SubmitButton />
         </div>
@@ -48,18 +63,19 @@ const UserProfileComponent = ({ name, image, metric, email }) => {
   );
 };
 
-function InputField({ label, placeholder, register, name }) {
+function InputField({ label, placeholder, register, name, validation, errors }) {
   return (
     <div key={placeholder?.replace(" ", "-")}>
       <label className="label">
         <span className="label-text">{label}</span>
       </label>
       <input
-        {...register(name)}
+        {...register(name, { ...validation })}
         type="text"
         placeholder={placeholder}
         className="input w-full max-w-xs"
       />
+      {errors[name] && <p>{errors[name].message}</p>}
     </div>
   );
 }
