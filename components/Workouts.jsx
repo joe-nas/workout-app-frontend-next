04a5cebd80089df53cd0d48671a58ac359eb22a6
joe-nas <pintac1984@gmail.com>
2023-11-22 21:18:45 +0100

    start on issue 3 and more dependencies

--- components/Workouts.jsx
+++ components/Workouts.jsx
@@ -5,6 +5,7 @@ import { deleteWorkout, getUserWorkouts } from "@/app/api/UserService";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { set } from "lodash";
+import { useForm } from "react-hook-form";
 
 const Workouts = () => {
   const { data: session } = useSession();
@@ -12,7 +13,8 @@ const Workouts = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [oauthId, setOauthId] = useState(null)
   const [jwt, setJwt] = useState(null)
-
+  const [editMode, setEditMode] = useState(false)
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -48,6 +50,37 @@ const Workouts = () => {
     };
   }
 
+  const submitEditedWorkout = (formData) => {
+    alert(JSON.stringify(formData));
+    setEditMode(false)
+  }
+
+  const editWorkout = async (oauthId, jwt, id) => {
+    setEditMode(true)
+  }
+
+
+  const EditWorkout = ({ data }) => {
+    return (
+      data.exercises.map((exercise) => (
+        <div className="" key={exercise.exerciseName}>
+          <h3 className="divider">{exercise.exerciseName}</h3>
+          <div className="overflow-x-auto">
+            {exercise.sets.map((set) => (
+              <form key={set.setNumber} onSubmit={handleSubmit(submitEditedWorkout)}>
+                <div className="flex justify-between">
+                  <input {...register("set")} type="number" defaultValue={set.setNumber} />
+                  <input {...register("reps")} type="number" defaultValue={set.reps} />
+                  <input {...register("weight")} type="number" defaultValue={set.weight} />
+                </div>
+              </form>
+            ))
+            }
+
+          </div>
+        </div >
+      )))
+  }
 
   return (
     <div className="">
@@ -60,36 +93,21 @@ const Workouts = () => {
               <h2 className="text-center">{workout.workoutName}</h2>
               <div className="flex justify-center">
 
-                <button className="btn btn-primary" onClick={() => deleteAndReload(oauthId, jwt, workout.id)}
-                >
-                  Delete Workout</button>
-              </div>
+                <button className="btn btn-primary" onClick={() => deleteAndReload(oauthId, jwt, workout.id)}>Delete</button>
 
-              {workout.exercises.map((exercise) => (
-                <div className="" key={exercise.exerciseName}>
-                  <h3 className="divider">{exercise.exerciseName}</h3>
-                  <div className="overflow-x-auto">
-                    <table className="table table-zebra">
-                      <thead>
-                        <tr>
-                          <th className="">Set Number</th>
-                          <th className="">Reps</th>
-                          <th className="">Weight</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {exercise.sets.map((set) => (
-                          <tr key={set.setNumber}>
-                            <td className="">{set.setNumber}</td>
-                            <td className="">{set.reps}</td>
-                            <td className="">{set.weight}</td>
-                          </tr>
-                        ))}
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              ))}
+
+                {editMode
+                  ? <button className="btn btn-primary mx-1" type="submit" onClick={handleSubmit(submitEditedWorkout)}>Submit</button>
+                  : <button className="btn btn-primary mx-1" onClick={() => editWorkout(oauthId, jwt, workout.id)}>Edit</button>
+                }
+
+
+                <button className="btn btn-primary" onClick={() => repeatWorkout(oauthId, jwt, workout.id)}>Repeat</button>
+              </div>
+              {editMode
+                ? <EditWorkout data={workout} />
+                : <ShowWorkout data={workout} />}
+
             </div>
           ))
         }
@@ -99,3 +117,38 @@ const Workouts = () => {
 }
 
 export default Workouts;
+
+
+const ShowWorkout = ({ data }) => {
+  return (
+    data.exercises.map((exercise) => (
+      <div className="" key={exercise.exerciseName}>
+        <h3 className="divider">{exercise.exerciseName}</h3>
+        <div className="overflow-x-auto">
+          <table className="table table-zebra">
+            <thead>
+              <tr>
+                <th className="">Set Number</th>
+                <th className="">Reps</th>
+                <th className="">Weight</th>
+              </tr>
+            </thead>
+            <tbody>
+              {exercise.sets.map((set) => (
+                <tr key={set.setNumber}>
+                  <td className="">{set.setNumber}</td>
+                  <td className="">{set.reps}</td>
+                  <td className="">{set.weight}</td>
+                </tr>
+              ))
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    ))
+
+  )
+}
+
+
